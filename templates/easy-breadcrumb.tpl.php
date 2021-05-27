f015d69c954f81ef7524fcc2a645feafb9ff5e7b
Laryn - CEDC.org <laryn@cedc.org>
2021-05-27 12:53:27 -0500

    Fix data-vocabulary.org schema deprecated error
    Fixes #13

--- templates/easy-breadcrumb.tpl.php
+++ templates/easy-breadcrumb.tpl.php
@@ -2,10 +2,13 @@
   <nav itemscope class="breadcrumb easy-breadcrumb" itemtype="<?php print $list_type; ?>" role="navigation">
     <h2 class="element-invisible">You are here</h2>
     <?php foreach ($breadcrumb as $i => $item): ?>
-      <?php print $item; ?>
-        <?php if (($i + 1) != $segments_quantity || $separator_ending): ?>
-           <span class="separator"><?php print $separator; ?></span>
-        <?php endif; ?>
+      <span itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem">
+        <?php print $item; ?>
+        <meta itemprop="position" content="<?php print $i; ?>" />
+      </span>
+      <?php if (($i + 1) != $segments_quantity || $separator_ending): ?>
+         <span class="separator"><?php print $separator; ?></span>
+      <?php endif; ?>
     <?php endforeach; ?>
   </nav>
 <?php endif; ?>
