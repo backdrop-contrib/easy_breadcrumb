c6b67be9ef5fc6619a0fc672802c43453b5d45c3
laryn <laryn@cedc.org>
2021-05-27 14:33:40 -0500

    Adjustments for crumbs with no link ...attempting to mollify Google Search Console.

--- templates/easy-breadcrumb.tpl.php
+++ templates/easy-breadcrumb.tpl.php
@@ -2,12 +2,18 @@
   <nav itemscope class="breadcrumb easy-breadcrumb" itemtype="<?php print $list_type; ?>" role="navigation">
     <h2 class="element-invisible">You are here</h2>
     <?php foreach ($breadcrumb as $i => $item): ?>
-      <span itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem">
-        <?php print $item; ?>
-        <meta itemprop="position" content="<?php print $i; ?>" />
-      </span>
+      <?php if ($is_link[$i]): ?>
+        <span itemprop="itemListElement" itemscope itemtype="https://schema.org/ListItem">
+          <?php print $item; ?>
+          <meta itemprop="position" content="<?php print $i; ?>" />
+        </span>
+      <?php else: ?>
+        <span itemprop="itemListElement">
+          <?php print $item; ?>
+        </span>
+      <?php endif; ?>
       <?php if (($i + 1) != $segments_quantity || $separator_ending): ?>
-         <span class="separator"><?php print $separator; ?></span>
+        <span class="separator"><?php print $separator; ?></span>
       <?php endif; ?>
     <?php endforeach; ?>
   </nav>
